f76a27306f9001bfbe83c34564a5cbc5c1d8081f
darkgithub1 <darkgithub16457@gmail.com>
2024-03-13 15:57:05 +0530

    created sign up page ui

--- api/utils/error.js
+++ api/utils/error.js
@@ -1,6 +1,7 @@
 
 // For handling custom error
 export const errorHandler=(statusCode,message)=>{
+    // Error() is inbuilt in js so new error as per our need
     const error=new Error()
     error.statusCode=statusCode
     error.message=message
